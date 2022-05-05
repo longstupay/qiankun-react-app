b103e9378ce62379a74b350fc9d746ec6dc102bf
cloudbase-100019521556 <791798834@qq.com>
2022-05-05 13:23:18 +0800

    5.5
    0..1.2

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# Docker命令  
+# 微前端qiankun个人网站应用
 
-## 推送到远程仓库
+## 使用技术  
+
+使用qiankun整合react,vue,angular构造个人网站。顺带学习figma和taiwindcss.
+docker 已配置好成基础的nginx镜像，可运行在公有云容器，也可运行在网站托管如github pages和[腾讯云web应用托管](https://console.cloud.tencent.com/webify/index) (国内CDN加速)
+
+### 更新状态  
+
+2022.5.5 react 的portfolio 页。vue和Ng基础应用联通
+
+## Docker部署命令  
+
+### 打包本地镜像
+
+```bash
+docker build -t yourAppname:tag .
+```
 
 ### 给本地镜像打tag  
 
+```bash
 docker tag local-image:tagname new-repo:tagname  
+```
+
+### 推送到远程仓库
+
+```bahs
+docker push image:tag
+docekr run -dp --restart=always imageId [option]
+```
--- src/componet/Head/index.tsx
+++ src/componet/Head/index.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 // import { LazyLoadImage } from 'react-lazy-load-image-component';
 // import 'react-lazy-load-image-component/src/effects/blur.css';
+import "./index.css";
+
+// 未拆分的组件
 
 function showMenu() {
   const menu = document.querySelector('#menu') as HTMLElement;
@@ -47,7 +50,7 @@ const Header = () => {
               <ul id="menu" className="nav-menus translate-y-[-200%] md:translate-y-0">
                 <a onClick={closeMenu} className="nav-link" href="#home">主页</a>
                 <a  onClick={closeMenu}  className="nav-link" href="#about">个人信息</a>
-                <a  onClick={closeMenu} className="nav-link" href="#portfolio">个人项目</a>
+                <a  onClick={closeMenu} className="nav-link" href="#portfolio">最近项目</a>
                 <a  onClick={closeMenu} className="nav-link" href="#contact">联系我</a>
               </ul>
               {/* Mobile Menu Button */}
@@ -94,7 +97,7 @@ const Header = () => {
               <p className="mb-3 opacity-70">
                 学历:本科
               </p>
-              <p className=" opacity-70"> 熟练运用html、css、JavaScript进行网页制作;掌握ES6,TypeScript;掌握NodeJS,可使用Nest,Midway开发后台管理;掌握vue,react,angular; 掌握Taro开发微信小程序</p>
+              <p className=" opacity-70"> 熟练运用html、css、JavaScript进行网页制作;掌握ES6,TypeScript;掌握NodeJS,可使用Nest,Midway开发后台管理;掌握vue,react,angular; 掌握Taro开发微信小程序;了解使用Figma设计B端页面；熟练使用docker部署常见应用</p>
             </div>
           </div>
           {/* my skill */}
@@ -131,7 +134,7 @@ const Header = () => {
         {/* portfolio section */}
         <section id="portfolio" className="pt-32">
           <h2 className="portfolio-heading">
-            个人项目
+            最近项目
           </h2>
           <ul className="w-full md:w-5/6 space-y-10 py-5">
             <li className="portfolio-card">
@@ -143,10 +146,10 @@ const Header = () => {
                     React App
                   </h2>
                   <p className="text-left">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    用于个人网站的主页信息，结合了react和tailwindcss，使用Figma构图。在微应用中是主应用。
                   </p>
                   <div className="mt-8 font-semibold text-white flex items-center gap-2">
-                    <a className="portfolio-link bg-cyan-400" href="#github-vue">
+                    <a className="portfolio-link bg-cyan-400" href="https://github.com/longstupay/qiankun-react-app">
                       <img className="w-5 h-5" src="https://qiankun-app.oss-cn-shenzhen.aliyuncs.com/github.svg" alt="github" />
                       github</a>
                     <Link className="portfolio-link bg-gray-600 dark:bg-black" to="/">
@@ -165,13 +168,13 @@ const Header = () => {
                     Angular App
                   </h2>
                   <p className="text-left">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    个人网站子应用，计划处理网站的邮件管理 留言页面，暂无。
                   </p>
                   <div className="mt-8 font-semibold text-white flex items-center gap-2">
-                    <a className="portfolio-link bg-cyan-400" href="#github-vue">
+                    <a className="portfolio-link bg-cyan-400 a-hover" href="#" onClick={e=>e.preventDefault()}>
                       <img className="w-5 h-5" src="https://qiankun-app.oss-cn-shenzhen.aliyuncs.com/github.svg" alt="github" />
                       github</a>
-                    <Link className="portfolio-link bg-gray-600 dark:bg-black" to="/app3">
+                    <Link  className="portfolio-link bg-gray-600 dark:bg-black" to="/app3">
                       <img className="w-5 h-5" src="https://qiankun-app.oss-cn-shenzhen.aliyuncs.com/web.svg" alt="website" />
                       website</Link>
                   </div>
@@ -187,10 +190,10 @@ const Header = () => {
                     Vue App
                   </h2>
                   <p className="text-left">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    vue子应用，计划配合Rss订阅展示咨询，或者展示后期python 抓取的数据。仍在建设。
                   </p>
                   <div className="mt-8 font-semibold text-white flex items-center gap-2">
-                    <a className="portfolio-link bg-cyan-400" href="#github-vue">
+                    <a className="portfolio-link bg-cyan-400 a-hover" href="#" onClick={e=>e.preventDefault()}>
                       <img className="w-5 h-5" src="https://qiankun-app.oss-cn-shenzhen.aliyuncs.com/github.svg" alt="github" />
                       github</a>
                     <Link className="portfolio-link bg-gray-600 dark:bg-black" to="/myapp2">
@@ -212,31 +215,31 @@ const Header = () => {
               <div className="space-y-12 mb-6">
                 <div>
                   <label className="text-white block mb-6 text-xl font-bold">Name</label>
-                  <input className="w-full border border-input-border bg-input px-4 py-4" />
+                  <input className="w-full border border-input-border bg-input px-4 py-4" placeholder="暂无实现，仍在规划" />
                 </div>
                 <div>
                   <label className="text-white block mb-6 text-xl font-bold">Email</label>
-                  <input type="email" className="w-full border border-input-border bg-input px-4 py-4" />
+                  <input type="email" className="w-full border border-input-border bg-input px-4 py-4" placeholder="暂无实现，仍在规划" />
                 </div>
                 <div>
                   <label className="text-white block mb-6 text-xl font-bold">Message</label>
-                  <textarea className="w-full border border-input-border bg-input px-4 py-4 h-56 resize-none" defaultValue={""} />
+                  <textarea className="w-full border border-input-border bg-input px-4 py-4 h-56 resize-none" placeholder="暂无实现，仍在规划" defaultValue={""} />
                 </div>
-                <button className="px-6 py-2 mb-4 bg-white text-black font-bold">发送</button>
+                <button className="px-6 py-2 mb-4 bg-white text-black font-bold a-hover">发送</button>
               </div>
               <div className="mt-12 mb-8">
                 {/* Contact info */}
-                <p className="text-secondary text-white">1824-997-xxxx</p>
+                <p className="text-secondary text-white">1824-997-4648</p>
                 <a href="mailto:email@mydomain.com" className="text-secondary underline mt-3 block text-white">76178271zyw@gmail.com</a>
                 {/* Socials */}
                 <div className="flex mt-20 space-x-6">
-                  {/* Skype */}
+                  {/* qq */}
                   <a href="#1">
-                    <svg className="w-8 h-8 lg:w-12 lg:h-12" width={50} height={50} viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M43.1494 28.252C43.3496 27.1533 43.457 26.0303 43.457 24.9072C43.457 22.417 42.9688 20 42.0068 17.7295C41.0791 15.5322 39.7461 13.5596 38.0518 11.8652C36.3716 10.1808 34.3799 8.83914 32.1875 7.91504C29.9121 6.95313 27.5 6.46484 25.0098 6.46484C23.8379 6.46484 22.6611 6.57715 21.5186 6.79687C19.9376 5.9591 18.176 5.51995 16.3867 5.51758C13.4717 5.51758 10.7275 6.65527 8.66699 8.71582C7.65021 9.72738 6.84403 10.9305 6.29507 12.2555C5.74611 13.5806 5.46526 15.0013 5.46875 16.4355C5.46875 18.291 5.94727 20.1172 6.84082 21.7285C6.66016 22.7734 6.5625 23.8428 6.5625 24.9072C6.5625 27.3975 7.05078 29.8145 8.0127 32.085C8.94043 34.2822 10.2686 36.2549 11.9629 37.9492C13.6572 39.6436 15.6299 40.9717 17.8272 41.8994C20.1025 42.8613 22.5147 43.3496 25.0049 43.3496C26.0889 43.3496 27.1729 43.252 28.2373 43.0615C29.8731 43.9844 31.7188 44.4775 33.6084 44.4775C36.5234 44.4775 39.2676 43.3447 41.3281 41.2793C43.3936 39.2188 44.5264 36.4746 44.5264 33.5596C44.5313 31.7041 44.0527 29.873 43.1494 28.252ZM25.083 36.2061C18.5303 36.2061 15.6006 32.9834 15.6006 30.5713C15.6006 29.3311 16.5137 28.4668 17.7734 28.4668C20.5762 28.4668 19.8535 32.4951 25.083 32.4951C27.7637 32.4951 29.2432 31.04 29.2432 29.5508C29.2432 28.6572 28.8037 27.6611 27.0361 27.2266L21.2061 25.7715C16.5137 24.5947 15.6592 22.0557 15.6592 19.6729C15.6592 14.7217 20.3223 12.8613 24.7022 12.8613C28.7354 12.8613 33.4912 15.0928 33.4912 18.0615C33.4912 19.3359 32.3877 20.0732 31.1279 20.0732C28.7354 20.0732 29.1748 16.7627 24.3555 16.7627C21.9629 16.7627 20.6397 17.8467 20.6397 19.3945C20.6397 20.9424 22.5293 21.4355 24.1699 21.8115L28.4863 22.7686C33.2129 23.8232 34.4092 26.582 34.4092 29.1797C34.4092 33.1982 31.3184 36.2061 25.083 36.2061Z" fill="white" /></svg>
+                    <svg className="w-8 h-8 lg:w-12 lg:h-12" viewBox="0 0 1025 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3352" width="200" height="200"><path d="M512.009337 0C229.23422 0 0 229.23422 0 511.990663c0 282.775117 229.23422 512.009337 512.009337 512.009337 282.775117 0 512.009337-229.23422 512.009337-512.009337C1024 229.23422 794.76578 0 512.009337 0zM801.26464 668.859701c-21.737567 18.637536-49.955319-61.346999-54.007769-49.040249-9.879014 29.935842-14.529061 49.936644-43.643208 82.505644-1.550016 1.736764 33.670819 14.473036 43.643208 41.62632 9.542867 26.03279 28.143053 67.285613-93.486477 80.227308-71.375413 7.582004-122.936772-38.022067-128.09104-37.592545-9.524192 0.84037-5.284993 0-15.51883 0-8.366349 0-8.926595 0.616271-16.807397 0-2.166287-0.168074-25.883391 37.592545-131.975416 37.592545-82.225521 0-103.514891-51.748108-86.987617-80.227308 16.545949-28.460526 44.128755-36.752175 40.244378-41.252822-19.141758-22.185764-32.363577-45.921544-40.244378-67.360312-1.942188-5.341017-3.585578-10.532635-4.874145-15.51883-2.987982-11.447705-25.883391 67.229588-50.459541 49.040249-24.576149-18.170664-22.391188-64.40968-6.480185-108.687834 16.060402-44.614302 56.491529-87.585213 56.939726-97.072055 1.624715-35.295534-3.473529-41.159448 0-50.422191 7.750078-20.766473 17.19957-12.792297 17.19957-23.567706 0-135.729068 100.863057-245.761494 225.293819-245.761494 124.412088 0 225.275145 110.032426 225.275145 245.761494 0 5.210293 13.520617 0 19.982128 23.567706 1.325917 4.874145 2.259661 23.66108 0.672296 50.422191-0.746995 12.848321 34.268415 28.497875 52.364379 97.072055C828.436598 628.764722 810.321959 661.109624 801.26464 668.859701z" p-id="3353" fill="#ffffff"></path></svg>
                   </a>
-                  {/* Twitter */}
+                  {/* qq */}
                   <a href="#2">
-                    <svg className="w-8 h-8 lg:w-12 lg:h-12" width={42} height={42} viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 0C9.40313 0 0 9.40313 0 21C0 32.5969 9.40313 42 21 42C32.5969 42 42 32.5969 42 21C42 9.40313 32.5969 0 21 0ZM31.0922 15.8297C31.1062 16.05 31.1062 16.2797 31.1062 16.5047C31.1062 23.3859 25.8656 31.3125 16.2891 31.3125C13.3359 31.3125 10.5984 30.4547 8.29219 28.9781C8.71406 29.025 9.11719 29.0437 9.54844 29.0437C11.9859 29.0437 14.2266 28.2188 16.0125 26.8219C13.725 26.775 11.8031 25.275 11.1469 23.2125C11.9484 23.3297 12.6703 23.3297 13.4953 23.1188C12.3175 22.8795 11.2588 22.2398 10.4991 21.3084C9.73949 20.377 9.32572 19.2113 9.32812 18.0094V17.9437C10.0172 18.3328 10.8281 18.5719 11.6766 18.6047C10.9633 18.1293 10.3784 17.4854 9.97365 16.7298C9.5689 15.9743 9.35683 15.1306 9.35625 14.2734C9.35625 13.3031 9.60938 12.4172 10.0641 11.6484C11.3714 13.2578 13.0028 14.5741 14.8522 15.5117C16.7016 16.4493 18.7275 16.9873 20.7984 17.0906C20.0625 13.5516 22.7063 10.6875 25.8844 10.6875C27.3844 10.6875 28.7344 11.3156 29.6859 12.3281C30.8625 12.1078 31.9875 11.6672 32.9906 11.0766C32.6016 12.2812 31.7859 13.2984 30.7031 13.9406C31.7531 13.8281 32.7656 13.5375 33.7031 13.1297C32.9953 14.1703 32.1094 15.0938 31.0922 15.8297Z" fill="white" /></svg>
+                    <svg className="w-8 h-8 lg:w-12 lg:h-12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4307" width="200" height="200"><path d="M512 1024C229.23 1024 0 794.77 0 512S229.23 0 512 0s512 229.23 512 512-229.23 512-512 512z m107.91-626.371H640c-20.09-94.744-115.566-162.962-225.304-162.962-124.002 0-222.696 86.04-222.696 198.607 0 65.097 34.572 115.492 89.43 156.94l-23.114 71.12 77.995-41.448a354.748 354.748 0 0 0 77.97 11.8h20.114a133.608 133.608 0 0 1-5.851-47.47 193.122 193.122 0 0 1 57.466-134.412 181.37 181.37 0 0 1 133.096-52.175h0.804z m-115.273-56.296c15.848 0 28.696 14.288 28.696 31.915s-12.848 31.915-28.696 31.915c-17.652 1.95-33.402-12.313-35.304-31.94 0-22.284 17.457-31.89 34.719-31.89h0.585z m-171.032 63.878c-17.555 1.463-33.012-12.653-34.938-31.89 1.926-19.212 17.383-33.329 34.938-31.89 16.042 0 29.062 14.287 29.062 31.915 0 17.603-13.02 31.89-29.062 31.89zM832 574.805c0-92.233-90.136-169.472-192-169.472-107.764 0-192 77.24-192 169.448 0 92.257 84.456 169.496 192 169.496a264.24 264.24 0 0 0 66.828-11.873L767.586 768l-17.408-59.538c49.42-35.596 81.017-83.286 81.017-133.852l0.805 0.195zM573.562 554.52c-10.435 0-18.895-9.484-18.895-21.187s8.46-21.211 18.895-21.211c11.727-1.39 22.308 7.997 23.771 21.114-1.39 13.214-11.97 22.698-23.771 21.284z m128 0.098c-10.435 0-18.895-9.509-18.895-21.212 0-11.751 8.46-21.26 18.895-21.26 11.727-1.414 22.308 7.997 23.771 21.139-2.194 12.921-12.58 22.04-24.259 21.333h0.488z" p-id="4308" fill="#ffffff"></path></svg>
                   </a>
                 </div>
               </div>
